37ae6b59aaf2a7a8d924de1c8b401279d18158b5
celgou <celine.goubaud@gmail.com>
2022-03-31 15:25:02 +0200

    Slutliga buggfixar
    Åtgärd av buggen som sade "undefined credits" och la till css.

--- U3 (21.03.2022)/students.js
+++ U3 (21.03.2022)/students.js
@@ -1,95 +1,102 @@
 "use strict";
 
-let allStudents = DATABASE.students;
-let allCourses = DATABASE.courses;
+let allStudents = DATABASE.students
+let allCourses = DATABASE.courses
 
-function renderStudent(student){
+function renderStudent (student) {
     let div = document.createElement("div");
     div.id = "student-div";
     div.innerHTML += `
-        <h2>${student.firstName} ${student.lastName} (total${totalCredits(student)} credits) </h2>
+        <h2>${student.firstName} ${student.lastName} (total ${studentCredits(student)} credits)</h2>
         <h3>Courses:</h3>
         <div id="course-div">
-        ${showCourses(student)}
+        ${allStudentCourses(student)}
         </div>
     `;
 
     return div;
-
 }
 
-function totalCredits(student){
-    let credits=[];
+function renderStudents (students) {
+    let studentsElement = document.getElementById("student-results");
 
-    for(let course of student.courses){
+    for (let student of students) {
+        let studentElement = renderStudent(student);
+        studentsElement.appendChild(studentElement);    
+    }
+}
+
+function studentCredits (student) {
+    let credits = [];
+
+    for (let course of student.courses) {
         credits.push(course.passedCredits);
     }
-    let creditSum= 0;
-    for(let i=0; i<credits.length; i++){
+
+    let creditSum = 0;
+    for (let i = 0; i < credits.length; i++) {
         creditSum += credits[i];
+        
     }
 
     return creditSum;
 }
 
+function allStudentCourses(student) {
+    let theCourses = [];
 
-function renderStudents(students){
-    let studentsElement = document.getElementById("student-results");
-
-    for (let student of students){
-        let studentElement = renderStudent(student);
-        studentsElement.appendChild(studentElement);
-    }
-}
-
-function showCourses(student){
-    let courses = [];
-
-    for(let i = 0; i < student.courses.length; i++){
-        let id = student.courses[i].courseId;
-        courses.push(allCourses[id]);
+    for (let i = 0; i < student.courses.length; i++) {
+        let Id = student.courses[i].courseId;
+        theCourses.push(allCourses[Id]);
     }
 
-    let courseBox = [];
-    for (let i=0; i<courses.length; i++){
-        let div=document.createElement("div");
+    let courseArray = [];
 
-        if (student.courses[i].passedCredits == allCourses[courses[i].courseId].totalCredits){
-            let info=div.innerHTML = `
-            <div class="done">
-                <h3 id="course-header">${allCourses[i].title}</h3>
-                <p id="course-paragraph">${student.courses[i].started.semester} ${student.courses[i].started.year} (${student.courses[i].passedCredits}
-                of ${allCourses[courses[i].courseId.totalCredits]} credits) </p>
-            </div>`
-            courseBox.push(info);
-        }else{
-                let info=div.innerHTML=`
-                <div class="notdone">
-                    <h3>${allCourses[i].title}</h3>
-                    <p>${student.courses[i].started.semester} ${student.courses[i].started.year} (${student.courses[i].passedCredits} of
-                        ${allCourses[courses[i].courseId].totalCredits} credits)</p>
-                </div>`
-                courseBox.push(info);
+    for (let i = 0; i < theCourses.length; i++) {
+        let div = document.createElement("div");
+
+        if (student.courses[i].passedCredits == 
+            allCourses[student.courses[i].courseId].totalCredits
+        ) {
+            let info = (div.innerHTML = `
+            <div id="done">
+            <h4 id="course-h">${allCourses[i].title}</h4>
+            <p id="course-p">${student.courses[i].started.semester} ${student.courses[i].started.year} 
+            ( ${student.courses[i].passedCredits} of 
+            ${allCourses[student.courses[i].courseId].totalCredits} credits)
+            </div>
+            `)
+            courseArray.push(info)
+        } else {
+            let info = (div.innerHTML = `
+            <div id="not-done">
+            <h4 id="course-h">${allCourses[i].title}</h4>
+            <p id="course-p">${student.courses[i].started.semester} ${student.courses[i].started.year} 
+            ( ${student.courses[i].passedCredits} of 
+            ${allCourses[student.courses[i].courseId].totalCredits} credits)
+            </div>
+            `)
+            courseArray.push(info)
         }
     }
-    return courseBox.toString().split(",").join("");
+
+    return courseArray.toString().split(",").join("");
 }
 
-function inputResult(){
+function inputResult () {
     let resultArray = [];
-    let input= document.getElementById("student-input");
+    let input = document.getElementById("student-input");
 
-    for(let i=0; i<allStudents.length; i++){
-        document.querySelector("#student-results").innerHTML="";
-        if ("" == input.value){
+    for (let i = 0; i < allStudents.length; i++) {
+        document.querySelector("#student-results").innerHTML = "";
+        if ("" == input.value) { 
             document.querySelector("#student-results").innerHTML = "";
-        } else if(allStudents[i].lastName.toLowerCase().includes(input.value.toLowerCase())){
+        } else if (allStudents[i].lastName.toLowerCase().includes(input.value.toLowerCase())) {
             resultArray.push(allStudents[i]);
         }
-        
     }
 
     renderStudents(resultArray);
 }
 
-document.getElementById("student-input").addEventListener("keyup",inputResult);
+document.getElementById("student-input").addEventListener("keyup", inputResult);
